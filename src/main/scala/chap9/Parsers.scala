97d5d1bc115eacd6c79c47a735d109244be11f7e
tmxhsk99 <tmxhsk99@naver.com>
2024-10-04 06:24:19 +0900

    feat: Parser 9.2 장

--- src/main/scala/chap9/Parsers.scala
+++ src/main/scala/chap9/Parsers.scala
@@ -22,13 +22,16 @@ trait Parsers[ParseError, Parser[+_]] { self => // 이에 의해 이 Parsers 인
   def succeed[A](a: A): Parser[A] =
     string("") map (_ => a)
 
+  def slice[A](p: Parser[A]): Parser[String]
   /**
    * 두 파서중 하나만 성공해도 성공하는 파서
    * ex)
    * run(or(string("abra"),string("cadabra")))("abra") == Right("abra")
    * run(or(string("abra"),string("cadabra")))({"cadabra") == Right("cadabra")
    */
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
+
+  def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
   /**
    * 문자열을 자동으로 Parser[String]으로 변환합니다.
@@ -44,7 +47,11 @@ trait Parsers[ParseError, Parser[+_]] { self => // 이에 의해 이 Parsers 인
    * 파서가 되풀이 되는 경우를 처리 하는 함수
    * run(listOfN(3,"ab"|"cad"))("ababcad") == Right(List("ab","ab","cad"))
    */
-  def listOfN[A](n:Int, p: Parser[A]): Parser[List[A]]
+  def listOfN[A](n:Int, p: Parser[A]): Parser[List[A]] =
+    if(n <= 0)
+      succeed(List())
+    else
+      map2(p, listOfN(n-1, p))(_ :: _)
 
   /**
    * Parser[String]으로 변환 가능한 모든 타입을 ParserOps[String]으로 변환합니다.
@@ -53,15 +60,27 @@ trait Parsers[ParseError, Parser[+_]] { self => // 이에 의해 이 Parsers 인
   ParserOps[String] = ParserOps(f(a))
 
 
-  // 9.2 가능한 대수 하나
-
-  /**
-   * 0개 이상의 문자를 인식 해서 그 개수를 돌려 주는 파서
-   */
-  def many[A](a:Parser[A]): Parser[List[A]]
+  def many[A](p:Parser[A]): Parser[List[A]] =
+    map2(p,many(p))(_ :: _) or succeed(List())
 
   def map[A,B] (a: Parser[A])(f: A => B ): Parser[B]
 
+  /**
+   * p 를 한 번 적용한 후, many(p)를 적용한다.
+   * map
+   */
+  def manay1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))((a,list) => a::list)
+
+  def product[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)]
+
+  /**
+   * product를 사용해 두 p1, p2를 순차적으로 적용한다.
+   * product의 결과 (A,B) 타입의 튜플이다.
+   * 이 튜플에 map을 적용하여 함수 f를 호출하고, 결과적으로 C 타입 값을 얻는다.
+   */
+  def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
+    product(p, p2) map {f.tupled}
 
   /**
    * 이 클래스는 Parser에 추가 메서드를 제공합니다.
@@ -72,8 +91,18 @@ trait Parsers[ParseError, Parser[+_]] { self => // 이에 의해 이 Parsers 인
    */
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2:Parser[B]): Parser[B] = self.or(p,p2)// trait 에 대한 or 메서드 참조의 모호함을 self를 이용해서 명시적으로 해소한다
+    def or[B>:A](p2:Parser[B]): Parser[B] = self.or(p,p2)// trait 에 대한 or 메서드 참조의 모호함을 self를 이용해서 명시적으로 해소한다
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
     def many = self.many(p)
+
+    def slice: Parser[String] = self.slice(p)
+    def **[B](p2: => Parser[B]): Parser[(A,B)] =
+      self.product(p, p2)
+    def product[B](p2: => Parser[B]): Parser[(A,B)] =
+      self.product(p,p2)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] =
+      self.flatMap(p)(f)
   }
 
 
