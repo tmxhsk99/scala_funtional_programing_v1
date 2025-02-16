1a57825b2922e8c93b1c5f80cef96a26c877ee10
tmxhsk99 <tmxhsk99@naver.com>
2025-02-16 09:29:55 +0900

    fix : 모나드형태의 IO로 수정

--- src/main/scala/chap13/BadIO.scala
+++ src/main/scala/chap13/BadIO.scala
@@ -1,41 +1,45 @@
-package chap13
+import scala.annotation.tailrec
 
-import chap13.BadIO._
+sealed trait IO[A] { self =>
+  def run: A
 
-
-object BadIO {
-  trait IO { self =>
-    def run: Unit
-    // ++ 연산자는 두 IO를 순차적으로 실행
-    def ++(io: IO): IO = new IO {
-      def run = {self.run; io.run} // 여기서 직접 run을 호출
-    }
-  }
-  object IO {
-    // 아무것도 하지 않는 빈 IO
-    def empty: IO = new IO { def run = {}}
+  def map[B](f: A => B): IO[B] = new IO[B] {
+    def run = f(self.run)
   }
 
-  def fahrenheitToCelsius(f: Double): Double =
-    (f -32) * 5.0/9.0
-
-
+  def flatMap[B](f: A => IO[B]): IO[B] = new IO[B] {
+    def run = f(self.run).run
+  }
 }
 
-object BadIoTest{
+object IO {
+  def unit[A](a: => A): IO[A] = new IO[A] {
+    def run = a
+  }
+
+  // println을 감싸는 함수
+  def printLn(msg: String): IO[Unit] =
+    unit(println(msg))
+}
+
+// 개선된 온도 변환 프로그램
+object ImprovedIOTest {
+  def fahrenheitToCelsius(f: Double): Double =
+    (f - 32) * 5.0/9.0
+
+  def printTemp(f: Double): IO[Unit] =
+    IO.printLn(s"${f}F = ${fahrenheitToCelsius(f)}C")
+
+
+  def printMany(count: Int, temp: Double): IO[Unit] = {
+    if (count <= 0) IO.unit(())
+    else for {
+      _ <- printTemp(temp)
+      _ <- printMany(count - 1, temp)
+    } yield ()
+  }
+
   def main(args: Array[String]): Unit = {
-    // 온도를 변환하고 출력하는 IO 생성
-    def printTemp(f: Double): IO = new IO {
-      def run = println(s"${f}F = ${fahrenheitToCelsius(f)}C")
-    }
-
-    // 여러 온도를 연속해서 출력하려고 할 때
-    def printMany(count: Int, temp: Double): IO = {
-      if (count <= 0) IO.empty
-      else printTemp(temp) ++ printMany(count - 1, temp)
-    }
-
-    // 이렇게 실행하면 큰 숫자에서 스택오버플로우 발생!
     printMany(10000, 98.6).run
   }
 }
